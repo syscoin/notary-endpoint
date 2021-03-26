61bb9f1b67cd318d9d7308f365a8ff60b1507331
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-03-26 12:17:24 -0700

    update to use syscointx-js
    remove syscoin core dependency

--- index.js
+++ index.js
@@ -1,59 +1,68 @@
-const express = require("express");
-const syscoin = require('@syscoin/syscoin-js');
-const cors = require('cors');
+const express = require('express')
+const syscointx = require('syscointx-js')
+const cors = require('cors')
+const bitcoin = require('bitcoinjs-lib')
+const WIF = 'cTTK8jcKcqffHJkoYGYxyM2LBwmDsvYimjXahzgdy94MRbupsKJF'
+const assetGuid = '341906151'
+const network = syscointx.utils.syscoinNetworks.testnet
 
-const syscoinClient = new syscoin.SyscoinRpcClient({host: 'localhost', rpcPort: '18370', username: 'username', password: 'password'});
-const notaryAddress = 'tsys1qm4vy6pdfyz4dflwwjstxqn30wlynrsl4rz4n4v';
-const asset_guid = '2102391361';
-
-var app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 app.listen(8081, () => {
-  console.log("Server running on port 8081");
-});
-app.post("/notarize", async (req, res, next) => {
-  // const reply = await getBlockchainInfo();
-  if(req.body && req.body.tx) {
-    const tx_hex = req.body.tx;
-    console.log("tx:", tx_hex);
-    const sighash = await getNotarySigHash(tx_hex);
-    console.log("sighash:", sighash);
-    const signedhash = await signHash(sighash);
-    console.log("signedhash:", signedhash);
-    const updatedtx = await assetTransactionNotarize(tx_hex, signedhash);
-    console.log("updatedtx:", updatedtx);
-    res.json({'sigs': [{ 'asset': asset_guid, 'sig': signedhash}]});
+  console.log('Server running on port 8081')
+})
+app.post('/notarize', async (req, res, next) => {
+  if (req.body && req.body.tx) {
+    const txHex = req.body.tx
+    const tx = bitcoin.Transaction.fromHex(txHex)
+    if (!tx || syscointx.utils.isAssetAllocationTx(tx.version) !== true) {
+      const error = new Error('Not an allocation transaction')
+      error.status = 404
+      next(error)
+      return
+    }
+    let assets = syscointx.getAssetsFromTx(tx)
+    if (!assets.has(assetGuid)) {
+      const error = new Error('Asset to be notarized not found in the transaction')
+      error.status = 404
+      next(error)
+      return
+    }
+    assets = new Map()
+    assets.set(assetGuid, {})
+    if (syscointx.getNotarizationSigHash(tx, assets, network) !== true) {
+      const error = new Error('Could not get notary sighash')
+      error.status = 404
+      next(error)
+      return
+    }
+    if (syscointx.signNotarizationSigHashesWithWIF(assets, WIF, network) !== true) {
+      const error = new Error('Could not sign notary sighash')
+      error.status = 403
+      next(error)
+      return
+    }
+    if (syscointx.addNotarizationSignatures(tx.version, assets, tx.outs) === -1) {
+      const error = new Error('Could not add notary signature')
+      error.status = 400
+      next(error)
+      return
+    }
+    const jsonOut = syscointx.createNotarizationOutput(assets)
+    res.json({ sigs: jsonOut })
+  } else {
+    const error = new Error('No transaction included in body')
+    error.status = 400
+    next(error)
   }
-});
-
-async function getBlockchainInfo() {
-  try {
-    return await syscoinClient.callRpc("getblockchaininfo", []).call();
-  } catch (e) {
-    console.log("ERR getChainTips", JSON.stringify(e.response.data.error));
-  }
-}
-
-async function signHash(sighash) {
-  try {
-    return await syscoinClient.callRpc("signhash",[notaryAddress, sighash] ).call();
-  } catch (e) {
-    console.log("ERR signhash", JSON.stringify(e.response.data.error));
-  }
-}
-
-async function getNotarySigHash(tx) {
-  try {
-    return await syscoinClient.callRpc("getnotarysighash", [tx, asset_guid]).call();
-  } catch (e) {
-    console.log("ERR getnotarysighash", JSON.stringify(e.response.data.error));
-  }
-}
-async function assetTransactionNotarize(tx, signedhash) {
-  try {
-    return await syscoinClient.callRpc("assettransactionnotarize", [tx, asset_guid, signedhash]).call();
-  } catch (e) {
-    console.log("ERR assettransactionnotarize", JSON.stringify(e.response.data.error));
-  }
-}
+})
+// error handler middleware
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).send({
+    error: {
+      status: error.status || 500,
+      message: error.message || 'Internal Server Error'
+    }
+  })
+})
