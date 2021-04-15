39b55abc7f2ed7fdb3b1216710a2016ccd366314
Willy Ko <willyk@syscoin.org>
2021-04-15 05:31:36 -0700

    Added input sanitization to blacklist

--- app/controllers/blacklist.ts
+++ app/controllers/blacklist.ts
@@ -1,8 +1,15 @@
 import Blacklist from "../models/blacklist_schema"
+const bitcoin = require("bitcoinjs-lib");
+const syscointx = require("syscointx-js");
+const network = syscointx.utils.syscoinNetworks.testnet;
+interface Error {
+    status?: number;
+}
 
 module.exports = {
 
     getBlacklist : function(req: any, res: any) {
+
         const blacklist = Blacklist.find((err: any, blacklist: any) => {
             if (err) {
                 res.send(err);
@@ -12,6 +19,15 @@ module.exports = {
         });
     },
     addBlacklist : function(req: any, res: any) {
+        try {
+            bitcoin.address.toOutputScript(req.body.address, network);
+        } catch (e) {
+            const errorType = "Invalid address";
+            const error = new Error(errorType);
+            error.status = 400;
+            res.send(error);
+            return;
+        }
         const blacklistEntry = new Blacklist(req.body);
         blacklistEntry.save((err: any) => {
             if (err) {
@@ -23,6 +39,15 @@ module.exports = {
     },
 
     deleteBlacklist : function(req: any, res: any) {
+        try {
+            bitcoin.address.toOutputScript(req.body.address, network);
+        } catch (e) {
+            const errorType = "Invalid address";
+            const error = new Error(errorType);
+            error.status = 400;
+            res.send(error);
+            return;
+        }
         const blacklistEntry = Blacklist.deleteOne(req.body, (err: any) => {
             if (err) {
                 res.send(err);
